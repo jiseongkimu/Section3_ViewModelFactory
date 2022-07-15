099ea51ed720ecf7435fdea84055e9d1381fa021
jiseongkimu <jiseongkimu@gmail.com>
2022-07-15 10:15:40 +0900

    +livaData

--- app/src/main/java/com/example/section3_viewmodelfactory/MainActivity.kt
+++ app/src/main/java/com/example/section3_viewmodelfactory/MainActivity.kt
@@ -21,6 +21,11 @@ class MainActivity : AppCompatActivity() {
         viewModelFactory = MainActivityViewModelFactory(125)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
 
+        // 메인 액티비티에서 count의 value를 observe한다.
+        // 뷰모델에서 private 변수를 대신 참조할 변수를 사용
+        // observe를 통해 countTotal(자동으로 get이 호출되면서 count에 접근)값의 변경을 감지
+        // 본문엔 데이터 변경에 따른 로직 처리. 여기서 it은 count의 value
+        // get의 역할을 자동으로 수행함
         viewModel.countTotal.observe(this, Observer {
             binding.countText.text = it.toString()
         })
--- app/src/main/java/com/example/section3_viewmodelfactory/MainActivityViewModel.kt
+++ app/src/main/java/com/example/section3_viewmodelfactory/MainActivityViewModel.kt
@@ -5,7 +5,10 @@ import androidx.lifecycle.ViewModel
 
 
 class MainActivityViewModel(startingTotal: Int) : ViewModel() {
+
     private var count = MutableLiveData<Int>()
+
+    // private 변수를 외부에서 참조하는 getter를 갖는 변수 선언
     val countTotal : LiveData<Int>
         get() = count
 
@@ -13,7 +16,10 @@ class MainActivityViewModel(startingTotal: Int) : ViewModel() {
         count.value = startingTotal
     }
 
+    // 직접 변수를 관찰할 것이기 때문에 count값을 리턴하는 get()은 필요가 없다.
+
     fun updatedCount(){
+        // 널 체크
         count.value = (count.value)?.plus(1)
     }
 }
